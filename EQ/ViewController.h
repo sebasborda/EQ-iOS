1ee67635a91245bfeefaa957ae85e53acc403154
yo <Seba@yos-Mac.local>
2013-06-05 18:11:59 -0700

    Initial Commit

---
+++ EQ/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  EQ
+//
+//  Created by yo on 6/5/13.
+//  Copyright (c) 2013 Sebastian Borda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
